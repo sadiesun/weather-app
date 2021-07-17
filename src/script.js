edccbbf6d9959ffdf67f670c7fd3bd34d8099b7f
sadiesun <86273915+sadiesun@users.noreply.github.com>
2021-07-17 15:54:50 -0400

    Added 6 day forecast API

--- src/script.js
+++ src/script.js
@@ -41,32 +41,38 @@ function displayWeather(response) {
 getForecast(response.data.coord);
 
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days= ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
 
+  return days[day];
+}
 function displayForecast (response){
-  console.log(response.data.daily);
+  let forecast= response.data.daily;
   let forecastElement=document.querySelector("#forecast")
-  let days=["Sun","Mon","Tues","Wed","Thu","Fri"];
   
 let forecastHTML= `<div class="row">`;
-days.forEach(function(day) {
+forecast.forEach(function(forecastDay , index) {
+  if (index < 6) {
 forecastHTML= forecastHTML +
 
 `<div class="col-2 day">
-<div class="weather-forecast-date">${day}</div>
+<div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
   <div class="card border-info mb-3" style="width: 7rem">
       <div class="card-body" id="sixday">
         <img
-      src="http://openweathermap.org/img/wn/10d@2x.png"
+      src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
       alt=""
-      width="75" />
+      width="75" /> 
     <div class="weather-forecast-temperatures">
-      <span class="weather-forecast-temperature-max"> 18° </span>
-      <span class="weather-forecast-temperature-min"> 12° </span>
+      <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+      <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
     </div>
       </div>
   </div>
 </div>
-`;
+`; }
 });
 forecastHTML= forecastHTML + `</div>`;
 forecastElement.innerHTML=forecastHTML;
